723a13d7bf130aeb0a0f7d1d3582ad42bb3de2c2
yuya <71yuya2@yahoo.ne.jp>
2024-03-07 01:30:12 +0900

    コミット2回目

--- components/table.tsx
+++ components/table.tsx
@@ -5,40 +5,82 @@ import RefreshButton from './refresh-button'
 
 export default async function Table() {
   const startTime = Date.now()
-  const users = await prisma.users.findMany()
+  // const users = await prisma.users.findMany()
+
+  const headers = [
+    {
+        date: 'Date',
+        matchname: 'Matchname',
+        team: 'Team',
+        place: 'Place',
+        result: 'Result',
+        members: 'Members',
+        formation: 'Formation',
+      },
+]
+
+  const users = [
+    {
+        id: 1,
+        date: '2024/3/7',
+        matchname: 'semifinal',
+        team: 'Ateam',
+        place: 'xxstatium',
+        result: '1-0',
+        members: 'aa',
+        formation: 'xxx',
+      },
+]
+  console.log(users)
   const duration = Date.now() - startTime
 
   return (
     <div className="bg-white/30 p-12 shadow-xl ring-1 ring-gray-900/5 rounded-lg backdrop-blur-lg max-w-xl mx-auto w-full">
       <div className="flex justify-between items-center mb-4">
-        <div className="space-y-1">
-          <h2 className="text-xl font-semibold">Recent Users</h2>
-          <p className="text-sm text-gray-500">
-            Fetched {users.length} users in {duration}ms
-          </p>
-        </div>
+        
         <RefreshButton />
       </div>
       <div className="divide-y divide-gray-900/5">
-        {users.map((user) => (
+
+      {headers.map((header) => (
           <div
-            key={user.name}
+            key={header.id}
             className="flex items-center justify-between py-3"
           >
             <div className="flex items-center space-x-4">
-              <Image
-                src={user.image}
-                alt={user.name}
-                width={48}
-                height={48}
-                className="rounded-full ring-1 ring-gray-900/5"
-              />
-              <div className="space-y-1">
-                <p className="font-medium leading-none">{user.name}</p>
-                <p className="text-sm text-gray-500">{user.email}</p>
+              
+              <div className="flex flex-row space-x-4">
+                <p className="font-medium">{header.date}</p>
+                <p className="font-medium">{header.matchname}</p>
+                <p className="font-medium">{header.team}</p>
+                <p className="font-medium">{header.place}</p>
+                <p className="font-medium">{header.result}</p>
+                <p className="font-medium">{header.members}</p>
+                <p className="font-medium">{header.formation}</p>
               </div>
             </div>
-            <p className="text-sm text-gray-500">{timeAgo(user.createdAt)}</p>
+
+          </div>
+        ))}
+
+        {users.map((user) => (
+          <div
+            key={user.id}
+            className="flex items-center justify-between py-3"
+          >
+            <div className="flex items-center space-x-4">
+              
+              <div className="flex flex-row space-x-4">
+                <p className="font-medium">{user.date}</p>
+                <p className="font-medium">{user.matchname}</p>
+                <p className="font-medium">{user.team}</p>
+                <p className="font-medium">{user.place}</p>
+                <p className="font-medium">{user.result}</p>
+                <p className="font-medium">{user.members}</p>
+                <p className="font-medium">{user.formation}</p>
+              </div>
+            </div>
+           
           </div>
         ))}
       </div>
